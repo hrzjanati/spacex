9d21901b8ec96562ddd4e2bf2215e503127088bc
𝙷𝚘𝚜𝚎𝚒𝚗 𝙹𝚊𝚗𝚊𝚝𝚒  <hrzjanati@s-Mac-mini.local>
2022-08-31 16:48:09 +0430

    first commit

---
+++ spacex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  spacex
+//
+//  Created by 𝙷𝚘𝚜𝚎𝚒𝚗 𝙹𝚊𝚗𝚊𝚝𝚒  on 8/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ spacex/spacexApp.swift
@@ -0,0 +1,17 @@
+//
+//  spacexApp.swift
+//  spacex
+//
+//  Created by 𝙷𝚘𝚜𝚎𝚒𝚗 𝙹𝚊𝚗𝚊𝚝𝚒  on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct spacexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
